f4d0194516db7137052194375885f5a4babdcf1a
AfterWind <spmushroom@hotmail.com>
2017-06-21 18:57:54 +0800

    Calendar施工中 - 2

--- src/components/calendar/calendar.ts
+++ src/components/calendar/calendar.ts
@@ -1,11 +1,112 @@
 import Vue from 'vue'
-import { Component, Prop } from 'vue-property-decorator'
+import * as moment from 'moment'
+import { Component, Prop, Watch } from 'vue-property-decorator'
+import { padLeft } from '~/utils/common'
+
+class Day {
+  public value: moment.Moment
+  public index: number
+
+  constructor({ index, value }: { index: number, value: moment.Moment }) {
+    this.index = index
+    this.value = value
+  }
+
+  get date() {
+    return this.value.date()
+  }
+}
+
+function fetchMonthCalendar(anchorDay: moment.Moment): Day[] {
+  let firstDayOfMonth = moment(anchorDay).date(1)
+  let daysCount = firstDayOfMonth.daysInMonth()
+  let weekDay = firstDayOfMonth.weekday()
+
+  return [...Array(42)].map(
+    (day, i) => new Day({
+      index: i,
+      value: moment(anchorDay).date(i - weekDay + 1)
+    })
+  )
+}
+
+function monthBackward(day: moment.Moment): moment.Moment {
+  return moment(day).month(day.month() - 1)
+}
+
+function monthForward(day: moment.Moment): moment.Moment {
+  return moment(day).month(day.month() + 1)
+}
 
 @Component
 export default class Calendar extends Vue {
-  private days = Array(35).fill(0).map((day, i) => ({ value: i + 1, selected: false }))
+  @Prop()
+  initDay: moment.Moment
 
-  selectDay(day: any) {
-    day.selected = !day.selected
+  @Prop()
+  dateSelected: (day: moment.Moment) => void
+
+  private weekdays: string[] = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sta', 'Sun']
+  private anchorDay: moment.Moment = moment()
+
+  private board1: Day[] = []
+  private board2: Day[] = []
+  private boardFlip: boolean = false
+
+  get title() {
+    return `${this.currentYear} - ${this.currentMonth}`
+  }
+
+  get currentYear() {
+    return this.anchorDay.year()
+  }
+
+  get currentMonth() {
+    return padLeft(this.anchorDay.month() + 1, 2, '0')
+  }
+
+  getDayStyle(day: Day): any {
+    return {
+      'day--highlight': this.isToday(day.value),
+      'day--disable': !this.isCurrentMonth(day.value)
+    }
+  }
+
+  isToday(day: moment.Moment): boolean {
+    return day.year() === this.initDay.year() &&
+      day.month() === this.initDay.month() &&
+      day.date() === this.initDay.date()
+  }
+
+  isCurrentMonth(day: moment.Moment): boolean {
+    return day.month() === this.anchorDay.month()
+  }
+
+  lastMonth() {
+    this.anchorDay = monthBackward(this.anchorDay)
+  }
+  nextMonth() {
+    this.anchorDay = monthForward(this.anchorDay)
+  }
+
+  selectDay(day: Day) {
+    // day.selected = !day.selected
+    this.dateSelected && this.dateSelected(day.value)
+  }
+
+  @Watch('initDay')
+  refresh(initDay: moment.Moment) {
+    this.anchorDay = moment(this.initDay)
+  }
+
+  @Watch('anchorDay')
+  init(anchorDay: moment.Moment) {
+    let board = fetchMonthCalendar(anchorDay)
+    this.boardFlip ? (this.board1 = board) : (this.board2 = board)
+    this.boardFlip = !this.boardFlip
+  }
+
+  mounted() {
+    if (this.initDay !== void 0) this.init(this.initDay)
   }
 }
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -15,6 +15,38 @@ export function idGen(length: number = 8): string {
 }
 
 /**
+ * 向字符串右侧填充指定长度的内容
+ * 
+ * @export
+ * @param {any} source 需要填充的字符串
+ * @param {number} num 填充后的长度
+ * @param {string} [fill=' '] 填充的内容，默认为空格
+ * @returns {string}  填充后的字符串
+ */
+export function padRight(source: any, num: number, fill: string = ' '): string {
+  if (typeof source !== 'string') source = source.toString()
+  if (source.length >= num) return source
+
+  return source.concat(fill.repeat(num - source.length))
+}
+
+/**
+ * 向字符串左侧填充指定长度的内容
+ * 
+ * @export
+ * @param {any} source 需要填充的字符串
+ * @param {number} num 填充后的长度
+ * @param {string} [fill=' '] 填充的内容，默认为空格
+ * @returns 填充后的字符串
+ */
+export function padLeft(source: any, num: number, fill: string = ' ') {
+  if (typeof source !== 'string') source = source.toString()
+  if (source.length >= num) return source
+
+  return fill.repeat(num - source.length).concat(source)
+}
+
+/**
  * 节流函数
  * 
  * @export
